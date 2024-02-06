1395d9524de9cc101e19ffc89d8910ea8867ef9d
SuDenDimon <94171603+SuDenDimon@users.noreply.github.com>
2024-02-06 15:26:27 +0200

    optimization

--- js/task-4.js
+++ js/task-4.js
@@ -1,10 +1,6 @@
 const getTotalBalanceByGender = (users, gender) => {
 
-    return users.map((user) => user) 
-
-    .filter((user) => user.gender === gender)
-
-    .toSorted((genderMale, genderFemale) => genderMale.name.localeCompare(genderFemale.gender))
+    return users.filter((user) => user.gender === gender)
 
     .reduce((intermediateBalance, user) => {
 
